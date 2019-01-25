8c87023f1dd3b28222a2536ccee7e8e800e68bef
Markss07 <markinfotech07@gmail.com>
2019-01-25 12:11:07 +0530

    Made Some Changes
    1. Added new gradle dependency named as EventBus
    2. Created new customized toolbar for App
    3. Added new style for Custome Toolbar
    4. Added new color scheme
    5. Added several dimension for toolbar
    6. Initialize the custome toolbar in MainActivity
    7. Added entry of custome toolbar in Manifest.xml

--- EventBusDemo/app/src/main/java/com/example/eventbusdemo/MainActivity.java
+++ EventBusDemo/app/src/main/java/com/example/eventbusdemo/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.eventbusdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView toolbar_Title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    public void initToolbar(){
+        toolbar = findViewById(R.id.toolbar);
+        toolbar_Title = (TextView) toolbar.findViewById(R.id.titleMain);
     }
 }
